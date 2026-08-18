3e6332ad3b54faeee4f82ce7df851adb56706113
ThatSpirit <lthekeaneone@gmail.com>
2026-08-18 16:42:03 +0100

    Enhance code readability with comments
    Added comments for clarity and improved code readability. Also decreased code size by removing an unnecessary variable.

--- Distance_Fan_Code.ino
+++ Distance_Fan_Code.ino
@@ -1,14 +1,13 @@
-#include "SR04.h"
+#include "SR04.h" // include ultrasonic sensor library
 #define TRIG_PIN 12
 #define ECHO_PIN 11
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long a;
+
+long a; // distance integer for the sensor
 
 #define ENABLE 5
 #define DIRA 3
 #define DIRB 4
-
-int i;
 
 void setup() {
   pinMode(ENABLE,OUTPUT);
@@ -19,15 +18,15 @@ void setup() {
 }
 
 void loop() {
-  a=sr04.Distance();
+  a=sr04.Distance(); // read distance
   Serial.println(a);
   if (a<10) { // adjust the min distance for the fan to turn on here
-    digitalWrite(ENABLE,HIGH); 
+    digitalWrite(ENABLE,HIGH); // start fan
     digitalWrite(DIRA,LOW); 
     digitalWrite(DIRB,HIGH);
     delay(500);
   } else{
-    digitalWrite(ENABLE,LOW);
+    digitalWrite(ENABLE,LOW); // stop fan
     delay(500);
   }
 }
